de8dabe7c133a1024a2390d38ad5c51faf47432e
mauroslucios <mauroslucios@gmail.com>
2025-05-27 08:10:13 -0300

    repositories do sistema

---
+++ src/main/java/com/ist/hdi/repository/EnfermidadeRepository.java
@@ -0,0 +1,11 @@
+package com.ist.hdi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ist.hdi.entities.Enfermidade;
+
+@Repository
+public interface EnfermidadeRepository extends JpaRepository<Enfermidade, Long>{
+
+}
