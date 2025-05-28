a26b1e8fd28da80e147c455953f0d4429fe93c2c
mauroslucios <mauroslucios@gmail.com>
2025-05-28 11:28:07 -0300

    Medico controller

---
+++ src/main/java/com/ist/hdi/Controllers/MedicoController.java
@@ -0,0 +1,102 @@
+package com.ist.hdi.Controllers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ist.hdi.Services.MedicoService;
+import com.ist.hdi.entities.Medico;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(path="/api/v1/medicos", produces="application/json")
+@CrossOrigin(origins = "*")
+public class MedicoController {
+
+	@Autowired
+	private MedicoService medicoService;
+	
+	@GetMapping
+	public ResponseEntity<List<Medico>> listarTodos(){
+		List<Medico> medicos = medicoService.listarTodos();
+		
+		// Adicona links HATEOAS a cada medico
+		medicos = medicos.stream().peek(medico -> {
+			Link selfLink = linkTo(methodOn(MedicoController.class).buscarPorId(medico.getId())).withSelfRel();
+			medico.add(selfLink);
+		}).collect(Collectors.toList());
+		return ResponseEntity.ok(medicos);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Medico> buscarPorId(@PathVariable Long id){
+		return medicoService.buscarPorId(id)
+				.map(medico -> {
+                    Link selfLink = linkTo(methodOn(MedicoController.class).buscarPorId(medico.getId())).withSelfRel();
+                    medico.add(selfLink);
+                    return ResponseEntity.ok(medico);
+                })
+                .orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<Medico> salvar(@Valid @RequestBody Medico medico){
+		Medico salvarMedico = medicoService.salvar(medico);
+		Link selfLink = linkTo(methodOn(MedicoController.class).buscarPorId(salvarMedico.getId())).withSelfRel();
+		salvarMedico.add(selfLink);
+		return ResponseEntity.ok(salvarMedico);
+	}
+	
+	@PutMapping(path = "/{id}", consumes = "application/json")
+	public ResponseEntity<Medico> atualizar(@PathVariable Long id,@Valid @RequestBody Medico medico) {
+		if(!medicoService.buscarPorId(id).isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		medico.setId(id);
+		Medico medicoAtualizado = medicoService.salvar(medico);
+		medicoAtualizado.add (linkTo(methodOn(MedicoController.class).buscarPorId(medicoAtualizado.getId())).withSelfRel());
+		return ResponseEntity.ok(medicoAtualizado);	
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id){
+		if(!medicoService.buscarPorId(id).isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Medico>> listarPorEspecialidade(
+	        @RequestParam(required = false) String especialidade) {
+	    
+	    List<Medico> medicos;
+
+	    if (especialidade != null) {
+	        medicos = medicoService.listarPorEspecialidade(especialidade);
+	    } else {
+	        medicos = medicoService.listarTodos();
+	    }
+
+	    return ResponseEntity.ok(medicos);
+	}
+
+	
+}
