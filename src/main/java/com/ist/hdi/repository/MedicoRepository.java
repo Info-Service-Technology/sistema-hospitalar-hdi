c06a2c5bff8df68733b86c1886c3c45ddb42d55c
mauroslucios <mauroslucios@gmail.com>
2025-05-30 20:45:59 -0300

    adicionando filtro por especialidade

--- src/main/java/com/ist/hdi/repository/MedicoRepository.java
+++ src/main/java/com/ist/hdi/repository/MedicoRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ist.hdi.entities.Medico;
+import com.ist.hdi.enums.Especialidade;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
 
-	List<Medico> findByEspecialidadeContainingIgnoreCase(String especialidade);
+	List<Medico> findByEspecialidade(Especialidade especialidade);
+
 
 }
