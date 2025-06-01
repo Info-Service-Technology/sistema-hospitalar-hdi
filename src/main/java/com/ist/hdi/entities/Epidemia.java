c8e0c222392f3dc8611fa62d63d35c31fbf95203
mauroslucios <mauroslucios@gmail.com>
2025-06-01 15:47:05 -0300

    entidade epidemia

--- src/main/java/com/ist/hdi/entities/Epidemia.java
+++ src/main/java/com/ist/hdi/entities/Epidemia.java
@@ -1,118 +1,127 @@
 package com.ist.hdi.entities;
 
+import com.ist.hdi.enums.SituacaoEpidemia;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
-import com.ist.hdi.enums.SituacaoEpidemia;
-
 @Entity
 @Table(name = "tb_epidemias")
 public class Epidemia {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Enfermidade relacionada à epidemia.
+     */
     @NotNull
     @ManyToOne
     @JoinColumn(name = "enfermidade_id")
     private Enfermidade enfermidade;
 
+    /**
+     * Região afetada pela epidemia.
+     */
     @NotNull
     private String regiao;
 
+    /**
+     * Data de início da epidemia.
+     */
     @NotNull
     private LocalDate inicio;
 
+    /**
+     * Data prevista ou real de fim da epidemia.
+     */
     @NotNull
     private LocalDate fim;
 
+    /**
+     * Status atual da epidemia.
+     */
     @NotNull
     @Enumerated(EnumType.STRING)
-    private SituacaoEpidemia status; // Ex: Ativa, Controlada
+    private SituacaoEpidemia status;
 
-    // getters e setters
+    // Construtores
     public Epidemia() {
-    	
     }
 
-	public Epidemia(Long id, @NotNull Enfermidade enfermidade, @NotNull String regiao, @NotNull LocalDate inicio,
-			@NotNull LocalDate fim, @NotNull SituacaoEpidemia status) {
-		super();
-		this.id = id;
-		this.enfermidade = enfermidade;
-		this.regiao = regiao;
-		this.inicio = inicio;
-		this.fim = fim;
-		this.status = status;
-	}
+    public Epidemia(Long id, @NotNull Enfermidade enfermidade, @NotNull String regiao, @NotNull LocalDate inicio,
+                    @NotNull LocalDate fim, @NotNull SituacaoEpidemia status) {
+        this.id = id;
+        this.enfermidade = enfermidade;
+        this.regiao = regiao;
+        this.inicio = inicio;
+        this.fim = fim;
+        this.status = status;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Enfermidade getEnfermidade() {
-		return enfermidade;
-	}
+    public Enfermidade getEnfermidade() {
+        return enfermidade;
+    }
 
-	public void setEnfermidade(Enfermidade enfermidade) {
-		this.enfermidade = enfermidade;
-	}
+    public void setEnfermidade(Enfermidade enfermidade) {
+        this.enfermidade = enfermidade;
+    }
 
-	public String getRegiao() {
-		return regiao;
-	}
+    public String getRegiao() {
+        return regiao;
+    }
 
-	public void setRegiao(String regiao) {
-		this.regiao = regiao;
-	}
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
 
-	public LocalDate getInicio() {
-		return inicio;
-	}
+    public LocalDate getInicio() {
+        return inicio;
+    }
 
-	public void setInicio(LocalDate inicio) {
-		this.inicio = inicio;
-	}
+    public void setInicio(LocalDate inicio) {
+        this.inicio = inicio;
+    }
 
-	public LocalDate getFim() {
-		return fim;
-	}
+    public LocalDate getFim() {
+        return fim;
+    }
 
-	public void setFim(LocalDate fim) {
-		this.fim = fim;
-	}
+    public void setFim(LocalDate fim) {
+        this.fim = fim;
+    }
 
-	public SituacaoEpidemia getStatus() {
-		return status;
-	}
+    public SituacaoEpidemia getStatus() {
+        return status;
+    }
 
-	public void setStatus(SituacaoEpidemia status) {
-		this.status = status;
-	}
+    public void setStatus(SituacaoEpidemia status) {
+        this.status = status;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    // equals e hashCode baseados em id
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Epidemia)) return false;
+        Epidemia that = (Epidemia) o;
+        return Objects.equals(getId(), that.getId());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Epidemia other = (Epidemia) obj;
-		return Objects.equals(id, other.id);
-	}
-    
-    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
